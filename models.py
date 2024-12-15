bbaf6cb143c04a939acbb7fe27a3e394ad6a639b
PinJhih <isaacwang29@gmail.com>
2024-12-15 14:04:32 +0800

    feat: Load Bert model and tokenizer

---
+++ models.py
@@ -0,0 +1,24 @@
+import torch
+import torch.nn as nn
+from transformers import BertTokenizer, BertForSequenceClassification
+
+
+class BertClassifier(nn.Module):
+    def __init__(self, bert_model):
+        super(BertClassifier, self).__init__()
+        self.encoder = bert_model
+
+    def forward(self, inputs):
+        inputs = {k: v for k, v in inputs.items()}
+        output = self.encoder(**inputs).logits
+        return output
+
+
+def load_model(model_name="google-bert/bert-large-uncased"):
+    # load pretrained bert model
+    bert = BertForSequenceClassification.from_pretrained(model_name)
+    tokenizer = BertTokenizer.from_pretrained(model_name)
+
+    # create BertClassifier
+    model = BertClassifier(bert)
+    return model, tokenizer
--- train.py
+++ train.py
@@ -1,8 +1,40 @@
+import torch
+from torch.utils.data import DataLoader
+
 import dataset
+import models
 
 if __name__ == "__main__":
+    device = "cuda"
+
     # load datasets
     data_path = "./data/email_classification.csv"
     train_set, val_set = dataset.load_data(data_path)
     print("Training   samples:", len(train_set))
     print("Validation samples:", len(val_set))
+
+    # load model
+    model, tokenizer = models.load_model()
+    model = model.to(device)
+
+    MAX_LENGTH = 128
+    def collate_fn(batch):
+        texts = [item[0] for item in batch]
+        labels = torch.stack([item[1] for item in batch])
+        encoded_inputs = tokenizer(
+            texts,
+            padding=True,
+            truncation=True,
+            max_length=MAX_LENGTH,
+            return_tensors="pt",
+        )
+        return encoded_inputs, labels
+
+    # create dataloader
+    val_loader = DataLoader(val_set, 4, shuffle=True, collate_fn=collate_fn)
+    with torch.no_grad():
+        model.eval()
+        for x, y in val_loader:
+            x, y = x.to(device), y.to(device)
+            outputs = model(x)
+        print(outputs.shape)
